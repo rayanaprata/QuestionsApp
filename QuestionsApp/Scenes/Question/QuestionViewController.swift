dce51a06e03165b63dea30f1699da722efb30b12
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-09-19 16:43:42 -0300

    validation correct and wrong question

--- QuestionsApp/Scenes/Question/QuestionViewController.swift
+++ QuestionsApp/Scenes/Question/QuestionViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class QuestionViewController: UIViewController, UITableViewDataSource {
+class QuestionViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
         
     let question = QuestionModel(question: "Qual nome do gato da Rayana?",
                                   answer: [
@@ -18,7 +18,8 @@ class QuestionViewController: UIViewController, UITableViewDataSource {
                                   ],
                                   answerCorrect: 3)
     
-    var questionSelected: String?
+    var questionSelected: Int?
+    var indexSelected: IndexPath?
     
     @IBOutlet weak var questionButton: UIButton!
     @IBOutlet weak var tableView: UITableView!
@@ -40,9 +41,28 @@ class QuestionViewController: UIViewController, UITableViewDataSource {
         setupUI()
     }
     
+    @IBAction func handlerButtonAnswer(_ sender: Any) {
+        if questionSelected != nil {
+                        
+            guard let indexSelected = indexSelected else {return}
+            let indexCorrected = IndexPath(row: question.answerCorrect, section: 0)
+            
+            if let cellCorrect = tableView.cellForRow(at: indexCorrected) as? AnswerCell {
+                cellCorrect.setStyle(.correct)
+            }
+            
+            if let cellSelected = tableView.cellForRow(at: indexSelected) as? AnswerCell, indexSelected.row != question.answerCorrect {
+                cellSelected.setStyle(.incorrect)
+            }
+            
+            questionButton.layer.backgroundColor = UIColor(red: 0.52, green: 0.59, blue: 1.00, alpha: 1.00).cgColor
+        }
+    }
+    
     func setupUI() {
         tableView.register(UINib(nibName: "AnswerCell", bundle: nil), forCellReuseIdentifier: "AnswerCell")
         tableView.dataSource = self
+        tableView.delegate = self
         tableView.reloadData() // esse reloadData vai fazer a tabela disparar os métodos de data source dela (métodos de construção) -> esses métodos só são chamados quando é chamado esse reloadData
     }
     
@@ -55,13 +75,7 @@ class QuestionViewController: UIViewController, UITableViewDataSource {
         let answer = question.answer[indexPath.row]
         
         if let cell = tableView.dequeueReusableCell(withIdentifier: "AnswerCell") as? AnswerCell {
-            
             let style: AnswerCell.Style = .blank
-            
-//            if questionSelected == question.answerCorrect {
-//                style = .correct
-//            }
-            
             cell.setup(title: answer, style: style)
             return cell
         }
@@ -69,8 +83,10 @@ class QuestionViewController: UIViewController, UITableViewDataSource {
         return UITableViewCell() // apenas uma garantia pro caso dele nao conseguir pegar a cell anterior
     }
     
-    @IBAction func handlerButtonNextPage(_ sender: Any) {
-        let viewController = ResultViewController()
-        present(viewController, animated: true, completion: nil)
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        indexSelected = indexPath
+        questionSelected = indexPath.row
     }
+    
+    
 }
--- QuestionsApp/Scenes/Question/View/Cells/AnswerCell.swift
+++ QuestionsApp/Scenes/Question/View/Cells/AnswerCell.swift
@@ -31,6 +31,10 @@ class AnswerCell: UITableViewCell {
         viewBackground.layer.cornerRadius = 20
         viewBackground.layer.borderWidth = 1
         
+       setStyle(style)
+    }
+    
+    func setStyle(_ style: Style) {
         switch style {
         case .blank:
             viewBackground.backgroundColor = .white
@@ -40,7 +44,6 @@ class AnswerCell: UITableViewCell {
             labelTitle.textColor = UIColor(red: 0.52, green: 0.59, blue: 1.00, alpha: 1.00)
             viewBackground.backgroundColor = UIColor(red: 0.90, green: 0.91, blue: 1.00, alpha: 1.00)
             viewBackground.layer.borderColor = UIColor(red: 0.52, green: 0.59, blue: 1.00, alpha: 1.00).cgColor
-
         
         case .correct:
             labelTitle.textColor = UIColor(red: 0.22, green: 0.77, blue: 0.24, alpha: 1.00)
@@ -54,7 +57,7 @@ class AnswerCell: UITableViewCell {
             
         }
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         viewBackground.backgroundColor = selected ? UIColor(red: 0.90, green: 0.91, blue: 1.00, alpha: 1.00) : .white
         viewBackground.layer.borderColor = selected ? UIColor(red: 0.52, green: 0.59, blue: 1.00, alpha: 1.00).cgColor : UIColor(red: 0.83, green: 0.83, blue: 0.83, alpha: 1.00).cgColor
