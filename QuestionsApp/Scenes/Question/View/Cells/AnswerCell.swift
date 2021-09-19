1a988ae61c8f51d37d7ff3ed5858d252ec06bae3
rayanaprata <rayanaprata@uol.com.br>
2021-09-19 11:31:43 -0300

    adding component cell

---
+++ QuestionsApp/Scenes/Question/View/Cells/AnswerCell.swift
@@ -0,0 +1,23 @@
+//
+//  AnswerCell.swift
+//  QuestionsApp
+//
+//  Created by Rayana Prata Neves on 18/09/21.
+//
+
+import UIKit
+
+class AnswerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
